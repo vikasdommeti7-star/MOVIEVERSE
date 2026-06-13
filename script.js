221482e456afe9c0c495709a88c8fb86a9e716bb
VIKAS <vikasdommeti7@gmail.com>
2026-06-13 22:54:38 +0530

    Add trailer links for specific movie titles

--- script.js
+++ script.js
@@ -5,13 +5,37 @@ document.getElementById("modalDesc").innerText = desc;
 document.getElementById("modalRating").innerText = rating;
 document.getElementById("modalPoster").src = image;
 
+let trailerLink = "";
+
+if(title === "Baahubali 2"){
+trailerLink = "https://www.youtube.com/watch?v=G62HrubdD6o";
+}
+else if(title === "RRR"){
+trailerLink = "https://www.youtube.com/watch?v=NgBoMJy386M";
+}
+else if(title === "Spider-Man"){
+trailerLink = "https://www.youtube.com/watch?v=JfVOs4VSpmA";
+}
+else if(title === "The Avengers"){
+trailerLink = "https://www.youtube.com/watch?v=eOrNdBpGMv8";
+}
+else if(title === "Fast & Furious"){
+trailerLink = "https://www.youtube.com/watch?v=uisBaTkQAEs";
+}
+else if(title === "Rockstar"){
+trailerLink = "https://www.youtube.com/watch?v=bD5FShPZdpw";
+}
+
+document.getElementById("trailerBtn").onclick = function(){
+window.open(trailerLink, "_blank");
+};
+
 document.getElementById("movieModal").style.display = "flex";
 }
 
 document.querySelector(".close").onclick = function(){
-
 document.getElementById("movieModal").style.display = "none";
-}
+};
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -30,7 +54,7 @@ movies.forEach(function(movie){
 let movieName = movie.querySelector("h3").textContent.toLowerCase();
 
 if(movieName.includes(searchValue)){
-movie.style.display = "block";
+movie.style.display = "";
 }
 else{
 movie.style.display = "none";
