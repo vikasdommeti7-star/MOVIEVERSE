20a5705956c8910d8d487e6b84c45006766b58fe
VIKAS <vikasdommeti7@gmail.com>
2026-06-13 23:12:16 +0530

    Change trailer links to embedded format and improve modal
    Updated trailer links to use embed format and modified modal behavior.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+```javascript
 function openMovie(title, desc, rating, image){
 
 document.getElementById("modalTitle").innerText = title;
@@ -8,36 +9,50 @@ document.getElementById("modalPoster").src = image;
 let trailerLink = "";
 
 if(title === "Baahubali 2"){
-trailerLink = "https://www.youtube.com/watch?v=G62HrubdD6o";
+trailerLink = "https://www.youtube.com/embed/G62HrubdD6o";
 }
 else if(title === "RRR"){
-trailerLink = "https://www.youtube.com/watch?v=NgBoMJy386M";
+trailerLink = "https://www.youtube.com/embed/NgBoMJy386M";
 }
 else if(title === "Spider-Man"){
-trailerLink = "https://www.youtube.com/watch?v=JfVOs4VSpmA";
+trailerLink = "https://www.youtube.com/embed/JfVOs4VSpmA";
 }
 else if(title === "The Avengers"){
-trailerLink = "https://www.youtube.com/watch?v=eOrNdBpGMv8";
+trailerLink = "https://www.youtube.com/embed/eOrNdBpGMv8";
 }
 else if(title === "Fast & Furious"){
-trailerLink = "https://www.youtube.com/watch?v=uisBaTkQAEs";
+trailerLink = "https://www.youtube.com/embed/uisBaTkQAEs";
 }
 else if(title === "Rockstar"){
-trailerLink = "https://www.youtube.com/watch?v=bD5FShPZdpw";
+trailerLink = "https://www.youtube.com/embed/bD5FShPZdpw";
 }
 
-document.getElementById("trailerBtn").onclick = function(){
-window.location.href = trailerLink;
-};
+const frame = document.getElementById("trailerFrame");
+
+if(frame){
+frame.src = trailerLink;
+}
 
 document.getElementById("movieModal").style.display = "flex";
 }
 
-document.querySelector(".close").onclick = function(){
-document.getElementById("movieModal").style.display = "none";
-};
-
 document.addEventListener("DOMContentLoaded", function(){
+
+const closeBtn = document.querySelector(".close");
+
+if(closeBtn){
+closeBtn.onclick = function(){
+
+document.getElementById("movieModal").style.display = "none";
+
+const frame = document.getElementById("trailerFrame");
+
+if(frame){
+frame.src = "";
+}
+
+};
+}
 
 const searchBox = document.querySelector(".search-box");
 
@@ -51,7 +66,8 @@ let movies = document.querySelectorAll(".movie-card");
 
 movies.forEach(function(movie){
 
-let movieName = movie.querySelector("h3").textContent.toLowerCase();
+let movieName =
+movie.querySelector("h3").textContent.toLowerCase();
 
 if(movieName.includes(searchValue)){
 movie.style.display = "";
@@ -65,11 +81,18 @@ movie.style.display = "none";
 });
 
 }
+
 const themeBtn = document.getElementById("themeBtn");
+
+if(themeBtn){
 
 themeBtn.onclick = function(){
 
 document.body.classList.toggle("light-mode");
 
 };
+
+}
+
 });
+```
